210ed6617d6b871982496262f5359b3750925aa6
Vasyl Shtoiko <vasylshtoiko@Vasyls-MacBook-Pro.local>
2025-10-31 21:51:42 +0100

    Updated migration scripts

---
+++ src/main/resources/db/migration-test/V2__insert_initial_users.sql
@@ -0,0 +1,5 @@
+INSERT INTO users (id, first_name, last_name, email, status)
+VALUES
+    (1, 'John', 'Doe', 'john.doe@example.com', 'ACTIVE'),
+    (2, 'Jane', 'Doe', 'johnSecond.doe@example.com', 'ACTIVE'),
+    (3, 'Jane', 'Doe', 'jane.doe@example.com', 'ACTIVE');
